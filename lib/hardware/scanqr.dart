c5a2151e874b9b6d62e1b725e8f884eb5e0b3b1c
geastfn <geastfnysps@gmail.com>
2023-11-06 21:08:06 +0700

    GEA MAPS SELESAI!!!!

--- lib/hardware/scanqr.dart
+++ lib/hardware/scanqr.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:tubespariwisata/data/wisata.dart';
 import 'package:tubespariwisata/hardware/scannerError.dart';
 import 'package:tubespariwisata/anotherPageLauncher/launcher.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
-class BarcodeScannerPageView extends StatefulWidget{
+class BarcodeScannerPageView extends StatefulWidget {
   const BarcodeScannerPageView({Key? key}) : super(key: key);
 
   @override
@@ -12,95 +14,134 @@ class BarcodeScannerPageView extends StatefulWidget{
 }
 
 class _BarcodeScannerPageViewState extends State<BarcodeScannerPageView>
-  with SingleTickerProviderStateMixin {
-    BarcodeCapture? barcodeCapture;
+    with SingleTickerProviderStateMixin {
+  BarcodeCapture? barcodeCapture;
 
-    @override
-    Widget build(BuildContext context){
-      return Scaffold(
-        backgroundColor: Colors.black,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: PageView(
+        children: [
+          cameraView(),
+          Container(),
+        ],
+      ),
+    );
+  }
 
-        body: PageView(
+  Widget cameraView() {
+    return Builder(
+      builder: (context) {
+        return Stack(
           children: [
-            cameraView(),
-            Container(),
-          ],
-        ),
-      );
-    }
-
-    Widget cameraView() {
-      return Builder(
-        builder: (context) {
-          return Stack(
-            children: [
-              MobileScanner(
-                startDelay: true,
-                controller: MobileScannerController(torchEnabled: false),
-                fit: BoxFit.contain,
-                errorBuilder: (context, error, child){
-                  return ScannerErrorWidget(error: error);
-                },
-                onDetect: (capture) => setBarcodeCapture(capture),
-              ),
-
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  alignment: Alignment.bottomCenter,
-                  height: 100,
-                  color: Colors.black.withOpacity(0.4),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Center(
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width - 120,
-                          height: 50,
-                          child: FittedBox(
-                            child: GestureDetector(
-                              onTap: () => getURLResult(),
-                              child: barcodeCaptureTextResult(context),
+            MobileScanner(
+              startDelay: true,
+              controller: MobileScannerController(torchEnabled: false),
+              fit: BoxFit.contain,
+              errorBuilder: (context, error, child) {
+                return ScannerErrorWidget(error: error);
+              },
+              onDetect: (capture) {
+                setBarcodeCapture(capture);
+                showModalBottomSheet(
+                    context: context,
+                    builder: (context) => (Container(
+                          margin: const EdgeInsets.all(16.0),
+                          child: FlutterMap(
+                            options: MapOptions(
+                              initialCenter: daftarWisata[int.parse(
+                                  capture.barcodes.first.rawValue.toString())],
+                              initialZoom: 18.0,
                             ),
+                            children: [
+                              TileLayer(
+                                urlTemplate:
+                                    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                                userAgentPackageName: 'com.example.app',
+                              ),
+                              MarkerLayer(
+                                markers: [
+                                  Marker(
+                                    point: daftarWisata[int.parse(capture
+                                        .barcodes.first.rawValue
+                                        .toString())],
+                                    width: 80,
+                                    height: 80,
+                                    child: const Icon(
+                                      Icons.location_pin,
+                                      color: Colors.red,
+                                      size: 40.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        )));
+              },
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                height: 100,
+                color: Colors.black.withOpacity(0.4),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Center(
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width - 120,
+                        height: 50,
+                        child: FittedBox(
+                          child: GestureDetector(
+                            onTap: () => getURLResult(),
+                            child: barcodeCaptureTextResult(context),
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              )
-            ],
-          );
-        },
-      );
-    }
+              ),
+            )
+          ],
+        );
+      },
+    );
+  }
 
-    Text barcodeCaptureTextResult(BuildContext context){
-      return Text(
-        barcodeCapture?.barcodes.first.rawValue ?? LabelTextConstant.scanQrPlaceHolderLabel,
-        overflow: TextOverflow.fade,
-        style: Theme.of(context).textTheme.headlineMedium!.copyWith(color: Colors.white),
-      );
-    }
+  Text barcodeCaptureTextResult(BuildContext context) {
+    return Text(
+      barcodeCapture?.barcodes.first.rawValue ??
+          LabelTextConstant.scanQrPlaceHolderLabel,
+      overflow: TextOverflow.fade,
+      style: Theme.of(context)
+          .textTheme
+          .headlineMedium!
+          .copyWith(color: Colors.white),
+    );
+  }
 
-    void setBarcodeCapture(BarcodeCapture capture){
-      setState(() {
-        barcodeCapture = capture;
-      });
-    }
+  void setBarcodeCapture(BarcodeCapture capture) {
+    setState(() {
+      barcodeCapture = capture;
+    });
+  }
 
-    void getURLResult() {
-      final qrCode = barcodeCapture?.barcodes.first.rawValue;
+  void getURLResult() {
+    final qrCode = barcodeCapture?.barcodes.first.rawValue;
 
-      if(qrCode != null){
-        copyToClipboard(qrCode);
-      }
-    }
-
-    void copyToClipboard(String text){
-      Clipboard.setData(ClipboardData(text: text));
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text(LabelTextConstant.txtonCopyingClipBoard)),
-      );
+    if (qrCode != null) {
+      copyToClipboard(qrCode);
     }
   }
+
+  void copyToClipboard(String text) {
+    Clipboard.setData(ClipboardData(text: text));
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text(LabelTextConstant.txtonCopyingClipBoard)),
+    );
+  }
+}
